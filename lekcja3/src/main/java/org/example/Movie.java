27af00865b439cbafe9613c9c04d14cacc7e19b2
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-20 00:03:42 +0100

    zadanie 3.2 poprawione

--- lekcja3/src/main/java/org/example/Movie.java
+++ lekcja3/src/main/java/org/example/Movie.java
@@ -2,10 +2,17 @@ package org.example;
 
 public class Movie {
 
-    String title;
-    String directorName;
-    String directorSurname;
-    int durationInMinutes;
-    int yearOfProduction;
+    private final String title;
+    private final String directorName;
+    private final String directorSurname;
+    private final int durationInMinutes;
+    private final int yearOfProduction;
 
+    public Movie(String title, String directorName, String directorSurname, int durationInMinutes, int yearOfProduction) {
+        this.title = title;
+        this.directorName = directorName;
+        this.directorSurname = directorSurname;
+        this.durationInMinutes = durationInMinutes;
+        this.yearOfProduction = yearOfProduction;
+    }
 }

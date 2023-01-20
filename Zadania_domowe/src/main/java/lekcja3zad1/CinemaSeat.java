99293d1f0cd4197387fe063836f13ff75f61fdb5
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-20 12:31:29 +0100

    poprawki3.2

--- Zadania_domowe/src/main/java/lekcja3zad1/CinemaSeat.java
+++ Zadania_domowe/src/main/java/lekcja3zad1/CinemaSeat.java
@@ -4,11 +4,9 @@ public class CinemaSeat {
     private final int number;
     private final int row;
     private boolean isTaken;
-
     public void setTaken(boolean taken) {
         isTaken = taken;
     }
-
     public CinemaSeat(int number, int row) {
         this.number = number;
         this.row = row;
